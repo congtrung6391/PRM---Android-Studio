6bbc43a52626106ba61889f20a83baf20c7d3729
congtrung6391 <congtrung6391@gmail.com>
2022-06-11 23:51:46 +0700

    lab 7

--- app/src/main/java/com/example/onlinetutor/DetailActivity.java
+++ app/src/main/java/com/example/onlinetutor/DetailActivity.java
@@ -1,21 +1,139 @@
 package com.example.onlinetutor;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
+    private static final int MY_PERMISSION_REQUEST_CODE_CALL_PHONE = 555;
+    private static final String LOG_TAG = "DetailActivity";
+
     private TextView courseNameTextView;
+    private Button callButton;
+    private Intent callIntent;
+
+    private String phoneNumber;
+
+    void onSetupComponent() {
+        courseNameTextView = findViewById(R.id.detail_course_name);
+        callButton = findViewById(R.id.call_tutor_button);
+    }
+
+    void onSetupData() {
+        Intent intent = getIntent();
+        String courseName = intent.getStringExtra("course_name");
+        phoneNumber = "+84921221833";
+        callButton.setText("Call tutor: " + phoneNumber);
+        courseNameTextView.setText(courseName);
+    }
+
+    void onSetupIntent() {
+        callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + phoneNumber));
+    }
+
+    @SuppressLint("MissingPermission")
+    private void callNow() {
+        try {
+            this.startActivity(callIntent);
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),"Your call failed... " + ex.getMessage(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+
+    void askPermissionAndCall() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) { // 23
+
+            // Check if we have Call permission
+            int sendSmsPermisson = ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.CALL_PHONE);
+
+            if (sendSmsPermisson != PackageManager.PERMISSION_GRANTED) {
+                // If don't have permission so prompt the user.
+                this.requestPermissions(
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        MY_PERMISSION_REQUEST_CODE_CALL_PHONE
+                );
+                return;
+            }
+        }
+        this.callNow();
+    }
+
+    void onSetupListener() {
+        this.callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askPermissionAndCall();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        courseNameTextView = findViewById(R.id.detail_course_name);
-        Intent intent = getIntent();
-        String courseName = intent.getStringExtra("course_name");
-        courseNameTextView.setText(courseName);
+        onSetupComponent();
+        onSetupData();
+        onSetupIntent();
+        onSetupListener();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_CODE_CALL_PHONE: {
+
+                // Note: If request is cancelled, the result arrays are empty.
+                // Permissions granted (CALL_PHONE).
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i( LOG_TAG,"Permission granted!");
+                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_LONG).show();
+
+                    this.callNow();
+                }
+                // Cancelled or denied.
+                else {
+                    Log.i( LOG_TAG,"Permission denied!");
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+
+    // When results returned
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_PERMISSION_REQUEST_CODE_CALL_PHONE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Action OK", Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Action Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Action Failed", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
